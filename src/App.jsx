47224982e9f1a844bff99e2ca3892f37d78f9d9f
Shanto Noor <shantonoor900@gmail.com>
2024-03-27 14:22:17 +0600

    daisyUi added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-rose-600">Hello world!</h1>
+    <button className="btn w-64 rounded-full">Button</button>
   );
 }
