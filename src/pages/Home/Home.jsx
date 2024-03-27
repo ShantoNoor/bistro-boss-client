64279ed6f98898c0c2fdf5dd6b2e87c20c2c5788
Shanto Noor <shantonoor900@gmail.com>
2024-03-27 16:00:04 +0600

    popular menus section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "./Banner";
 import Catagory from "./Catagory";
+import PopularMenu from "./PopularMenu";
 
 const Home = () => {
   return (
     <>
       <Banner />
       <Catagory />
+      <PopularMenu />
     </>
   );
 };
