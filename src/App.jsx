73b1ece5e94e77b33e8d43e92750de687adc25f6
Shanto Noor <shantonoor900@gmail.com>
2024-03-27 14:32:56 +0600

    Home page crated

--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-export default function App() {
-  return (
-    <button className="btn w-64 rounded-full">Button</button>
-  );
-}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./layouts/Main.jsx";
+import Home from "./pages/Home.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
