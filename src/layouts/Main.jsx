9eb6544b70b2341d95676503af8f7ce318fb3317
Shanto Noor <shantonoor900@gmail.com>
2024-03-27 14:53:55 +0600

    NavBar added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/shared/Footer";
+import NavBar from "../pages/shared/NavBar";
 
 const Main = () => {
   return (
     <>
+      <NavBar />
       <Outlet />
       <Footer />
     </>
