a45ca932e9a6e89554857d45839849464b002101
Shanto Noor <shantonoor900@gmail.com>
2024-03-28 15:09:47 +0600

    testimonials added

--- src/pages/Home/Testimonials.jsx
+++ src/pages/Home/Testimonials.jsx
@@ -8,6 +8,10 @@ import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 import { useEffect, useState } from "react";
 
+import { Rating } from "@smastrom/react-rating";
+
+import "@smastrom/react-rating/style.css";
+
 const Testimonials = () => {
   const [reviews, setReviews] = useState([]);
 
@@ -27,10 +31,15 @@ const Testimonials = () => {
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
         {reviews.map((review) => (
           <SwiperSlide key={review._id}>
-           <div className="p-4 text-center space-y-4">
-           <p>{review.details}</p>
-            <h6 className="text-2xl text-orange-400">{review.name}</h6>
-           </div>
+            <div className="p-6 mx-24 flex flex-col justify-center items-center gap-3">
+              <Rating
+                style={{ maxWidth: 180 }}
+                value={review.rating}
+                readOnly
+              />
+              <p>{review.details}</p>
+              <h6 className="text-2xl text-orange-400">{review.name}</h6>
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
